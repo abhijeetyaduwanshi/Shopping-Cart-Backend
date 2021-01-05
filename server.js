765a70748cce4f26f12430a66fd7e55ba68f4cec
Abhijeet Yaduwanshi <abhijeetyaduwanshi@gmail.com>
2021-01-05 18:24:19 -0500

    added cors and body parser

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const categoryRoute = require('./routes/category.route');
 const navigationRoute = require('./routes/navigation.route');
@@ -7,6 +9,8 @@ const productRoute = require('./routes/product.route');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(cors());
 app.use(express.static(path.join(`${__dirname}/dist/shoppingCartBackend`)));
 app.use('/api/categories', categoryRoute);
 app.use('/api/navigation', navigationRoute);
